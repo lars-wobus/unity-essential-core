c06b98f3b5b83110bdadabf524593ada95b8def8
Lars Wobus <lars.wobus@web.de>
2018-09-03 21:34:49 +0200

    Simple wrapper to debug arrays

---
+++ EssentialCore/Assets/Essential/Core/Scripts/Wrapper/DebugWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+namespace Essential.Core.Extensions
+{
+    public static class DebugWrapper
+    {
+        public static void LogArray<T>(IEnumerable<T> array)
+        {
+            Debug.Log(string.Join("", array.ToList().ToList().ConvertAll( element => element.ToString()).ToArray()));
+        }
+    }
+    
+}
+   
+#endif
