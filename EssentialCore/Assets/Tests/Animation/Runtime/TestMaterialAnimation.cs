bdfe36da25e973fe0ee0fa121ac9a451bab85e8b
Lars Wobus <lars.wobus@web.de>
2018-09-05 23:49:51 +0200

    Assemblies + first runtime time for MaterialAnim.

---
+++ EssentialCore/Assets/Tests/Animation/Runtime/TestMaterialAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using Rapid.Animation;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests.Animation.Runtime
+{
+	public class TestMaterialAnimation {
+
+		private GameObject DummyGameObject { get; set; }
+	
+		[SetUp]
+		public void Setup()
+		{
+			DummyGameObject = new GameObject();
+		}
+	
+		[UnityTest]
+		public IEnumerator Should_ThrowNullReferenceException_When_MaterialWasNotSet()
+		{
+			LogAssert.Expect(LogType.Exception, "NullReferenceException: Material was null");
+			DummyGameObject.AddComponent<MaterialAnimation>();
+			yield return null;
+		}
+	}
+}
