ebd9a0d0360fad743f5ba3e09e823fe1b9af022e
Lars Wobus <lars.wobus@web.de>
2018-09-11 22:25:48 +0200

    Mark AnimationPlayer as obsolete
    - Instead the Playable API will be combined
    with custom scripts in the future

--- EssentialCore/Assets/Essential/Core/Scripts/Animation/AnimationPlayer.cs
+++ EssentialCore/Assets/Essential/Core/Scripts/Animation/AnimationPlayer.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Rapid.Animation
 {
+    [Obsolete]
     public class AnimationPlayer : IAnimationPlayer
     {
         [SerializeField] private int _counter;
