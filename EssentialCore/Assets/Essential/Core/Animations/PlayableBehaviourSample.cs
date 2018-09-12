65a2e9b38b2ae17dc582bc2acabe674d3926801a
Lars Wobus <lars.wobus@web.de>
2018-09-12 22:04:01 +0200

    Get rid of playable in PlayableBehaviourSample

--- EssentialCore/Assets/Essential/Core/Animations/PlayableBehaviourSample.cs
+++ EssentialCore/Assets/Essential/Core/Animations/PlayableBehaviourSample.cs
@@ -1,47 +1,7 @@
-﻿using UnityEngine;
+﻿using Essential.Core.Animation;
+using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Playables;
-
-public class BlenderPlayableBehaviour : PlayableBehaviour
-{
-    private float _time;
-    private float Time
-    {
-        get { return _time; }
-        set
-        {
-            _time = value;
-            Debug.Log(value.ToString());
-        }
-    }
-
-    public override void PrepareFrame(Playable playable, FrameData info)
-    {
-        Time += info.deltaTime;
-        Debug.Log(playable.GetTime().ToString()+" "+playable.GetDuration().ToString());
-    }
-    
-    public override void OnGraphStart(Playable playable)
-    {
-        Debug.Log("OnGraphStart");
-    }
-
-    // Called when the owning graph stops playing
-    public override void OnGraphStop(Playable playable) {
-        Debug.Log("OnGraphStop");
-    }
-
-    // Called when the state of the playable is set to Play
-    public override void OnBehaviourPlay(Playable playable, FrameData info)
-    {
-        Debug.Log("OnBehaviourPlay");
-    }
-
-    // Called when the state of the playable is set to Paused
-    public override void OnBehaviourPause(Playable playable, FrameData info) {
-        Debug.Log("OnBehaviourPause");
-    }
-}
 
 public class PlayableBehaviourSample : MonoBehaviour
 {
@@ -53,7 +13,7 @@ public class PlayableBehaviourSample : MonoBehaviour
 
         var animOutput = AnimationPlayableOutput.Create(m_Graph, "AnimationOutput", GetComponent<Animator>());
 
-        var blenderPlayable = ScriptPlayable<BlenderPlayableBehaviour>.Create(m_Graph, 1);
+        var blenderPlayable = ScriptPlayable<CustomPlayable>.Create(m_Graph, 1);
 
         animOutput.SetSourcePlayable(blenderPlayable);
         
