7c3d103bc0e7ce890874a578eb60288db3284783
Lars Wobus <lars.wobus@web.de>
2018-09-13 21:16:22 +0200

    Remove debug logs

--- EssentialCore/Assets/Essential/Core/Scripts/Animation/CustomPlayable.cs
+++ EssentialCore/Assets/Essential/Core/Scripts/Animation/CustomPlayable.cs
@@ -17,31 +17,31 @@ namespace Essential.Core.Animation
 		// Called when the owning graph starts playing
 		public override void OnGraphStart(Playable playable)
 		{
-			Debug.Log("OnGraphStart");
+			//Debug.Log("OnGraphStart");
 			GraphStarted?.Invoke();
 		}
 
 		// Called when the owning graph stops playing
 		public override void OnGraphStop(Playable playable) {
-			Debug.Log("OnGraphStop");
+			//Debug.Log("OnGraphStop");
 			GraphStopped?.Invoke();
 		}
 
 		// Called when the state of the playable is set to Play
 		public override void OnBehaviourPlay(Playable playable, FrameData info) {
-			Debug.Log("OnBehaviourPlay");
+			//Debug.Log("OnBehaviourPlay");
 			BehaviourPlayed?.Invoke();
 		}
 
 		// Called when the state of the playable is set to Paused
 		public override void OnBehaviourPause(Playable playable, FrameData info) {
-			Debug.Log("OnBehaviourPause");
+			//Debug.Log("OnBehaviourPause");
 			BehaviourPaused?.Invoke();
 		}
 
 		// Called each frame while the state is set to Play
 		public override void PrepareFrame(Playable playable, FrameData info) {
-			Debug.Log($"PrepareFrame {playable.GetTime().ToString()} {playable.GetDuration().ToString()}");
+			//Debug.Log($"PrepareFrame {playable.GetTime().ToString()} {playable.GetDuration().ToString()}");
 			ProgressChanged?.Invoke(playable.GetTime() / playable.GetDuration());
 		}
 	}
