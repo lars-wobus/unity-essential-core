8370f50c550c2d6f38adfa2ba0ea79055c9ca2ab
Lars Wobus <lars.wobus@web.de>
2018-09-15 10:12:46 +0200

    Scripts to animate texture tiling
    - script are inheriting from
    TextureOffsetAnimationBase

---
+++ EssentialCore/Assets/Essential/Core/Scripts/Animation/MainTextureScalingAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Essential.Core.Animation
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// Animate main texture tiling of Material.
+	/// </summary>
+	public class MainTextureScalingAnimation : TextureOffsetAnimationBase
+	{
+		/// <summary>
+		/// Keep texture scale from start.
+		/// </summary>
+		private Vector2 _mainTextureDefaultScale;
+
+		private void Start()
+		{
+			_mainTextureDefaultScale = Material.mainTextureScale;
+		}
+		
+		public override void SetProgress(double progress)
+		{
+			Material.mainTextureScale = _mainTextureDefaultScale + Alteration * (float) progress;
+		}
+		
+		/// <summary>
+		/// Revert changes on application end, otherwise version control will mark Material as modified.
+		/// </summary>
+		private void OnApplicationQuit()
+		{
+			Material.mainTextureScale = _mainTextureDefaultScale;
+		}
+	}
+}
---
+++ EssentialCore/Assets/Essential/Core/Scripts/Animation/TextureScalingAnimation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Essential.Core.Animation
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// Animate tiling of texture bound in shader. 
+	/// </summary>
+	public class TextureScalingAnimation : TextureOffsetAnimationBase
+	{
+		/// <summary>
+		/// Specifies which texture bound in shadder is affected by scale changes.
+		/// </summary>
+		[SerializeField] private string _shaderVariable = "_MainTex";
+		
+		/// <summary>
+		/// Keep texture scale from start.
+		/// </summary>
+		private Vector2 _mainTextureDefaultScale;
+
+		private void Start()
+		{
+			_mainTextureDefaultScale = Material.mainTextureScale;
+		}
+
+		public override void SetProgress(double progress)
+		{
+			Material.SetTextureScale(_shaderVariable, _mainTextureDefaultScale + Alteration * (float) progress);
+		}
+		
+		/// <summary>
+		/// Revert changes on application end, otherwise version control will mark Material as modified.
+		/// </summary>
+		private void OnApplicationQuit()
+		{
+			Material.mainTextureScale = _mainTextureDefaultScale;
+		}
+	}
+}
