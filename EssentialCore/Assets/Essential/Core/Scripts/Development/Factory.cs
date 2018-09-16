900d227fd16f524d3a18becea8107149250f1055
Lars Wobus <lars.wobus@web.de>
2018-09-16 23:05:06 +0200

    Temporary test for scriptable objects
    - those objects could work as a registry
    and could be used as predefined
    factories containing alot of prefab objects
    - registries might be not very useful in
    other scenes, but factories could bring
    some advantages as well as disadvantages
    - scriptable objects allow to save progress
    states of users during the executation
    as well as on application quit ->
    recovering last states should become much
    easier

---
+++ EssentialCore/Assets/Essential/Core/Scripts/Development/Factory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Essential.Core.Development
+{
+	[CreateAssetMenu(fileName = "Factory_", menuName = "Development/Factory", order = 1)]
+	public class Factory : ScriptableObject
+	{
+		[SerializeField] private List<GameObject> _prefabs;
+		[SerializeField] private GameObject _parent;
+
+		public void Instantiate()
+		{
+			_parent = GameObject.Find("_Dynamic");
+			Instantiate(_prefabs.ElementAt(0), _parent.transform);
+		}
+	}
+}
--- EssentialCore/Assets/Essential/Core/Scripts/Development/Subscriber.cs
+++ EssentialCore/Assets/Essential/Core/Scripts/Development/Subscriber.cs
@@ -5,9 +5,11 @@ namespace Essential.Core.Development
 	public class Subscriber : MonoBehaviour
 	{
 		[SerializeField] private Registry _registry;
+		[SerializeField] private Factory _factory;
 
 		private void Start () {
 			_registry.Register(gameObject);
+			_factory.Instantiate();
 		}
 
 		private void OnApplicationQuit()
