924b12868abb12724162f11b6b16cf4741d9bdeb
Lars Wobus <lars.wobus@web.de>
2018-09-16 22:49:58 +0200

    Temp. feature to test abilities of scriptableObj

---
+++ EssentialCore/Assets/Essential/Core/Scripts/Development/Registry.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Essential.Core.Development
+{
+	[CreateAssetMenu(fileName = "Registry_", menuName = "Development/Registry", order = 1)]
+	public class Registry : ScriptableObject
+	{
+		[SerializeField] private List<GameObject> _subscribers;
+
+		public void Register(GameObject gameObject)
+		{
+			_subscribers.Add(gameObject);
+		}
+		
+		public void Unregister(GameObject gameObject)
+		{
+			//_subscribers.Remove(gameObject);
+		}
+	}
+}
---
+++ EssentialCore/Assets/Essential/Core/Scripts/Development/Subscriber.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Essential.Core.Development
+{
+	public class Subscriber : MonoBehaviour
+	{
+		[SerializeField] private Registry _registry;
+
+		private void Start () {
+			_registry.Register(gameObject);
+		}
+
+		private void OnApplicationQuit()
+		{
+			_registry.Unregister(gameObject);
+		}
+	}
+}
