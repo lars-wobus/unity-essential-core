cce5d919386620d7e86c5f9c91bec1f1f502e151
Lars Wobus <lars.wobus@web.de>
2018-09-18 23:23:33 +0200

    Scripts to toggle gameobjects and their components

---
+++ EssentialCore/Assets/Essential/Core/Scripts/Utils/ToggleComponents.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Essential.Core.Utils
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// MonoBehaviour providing a method to toggle states of Components.
+	/// </summary>
+	/// <remarks>
+	/// Not every component comes with an 'enabled' property.
+	/// </remarks>
+	public class ToggleComponents : MonoBehaviour
+	{
+		/// <summary>
+		/// Persistent Components.
+		/// </summary>
+		[SerializeField] private Component[] _components;
+		
+		/// <summary>
+		/// Toggle 'enabled' properties of persistent Components.
+		/// </summary>
+		public void Toggle()
+		{
+			foreach (var component in _components)
+			{
+				var propertyInfo = component.GetType().GetProperty("enabled");
+				if (propertyInfo == null)
+				{
+					continue;
+				}
+
+				var enabled = (bool) propertyInfo.GetValue(component, null);
+				propertyInfo.SetValue(component, !enabled);
+			}
+		}
+	}
+}
---
+++ EssentialCore/Assets/Essential/Core/Scripts/Utils/ToggleGameObjects.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Essential.Core.Utils
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// MonoBehaviour providing a method to toggle active states of GameObjects.
+	/// </summary>
+	public class ToggleGameObjects : MonoBehaviour
+	{
+		/// <summary>
+		/// Persistent GameObjects.
+		/// </summary>
+		[SerializeField] private GameObject[] _gameObjects;
+
+		/// <summary>
+		/// Toggle active states of persistent GameObjects.
+		/// </summary>
+		public void Toggle()
+		{
+			foreach (var go in _gameObjects)
+			{
+				go.SetActive(!go.activeSelf);
+			}
+		}
+	}
+}
