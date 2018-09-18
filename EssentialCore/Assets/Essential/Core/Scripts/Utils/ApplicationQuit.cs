78ebe1949df659739e3fa3cbd905c5613655a685
Lars Wobus <lars.wobus@web.de>
2018-09-18 22:19:51 +0200

    MonoBehaviour to quit the application

---
+++ EssentialCore/Assets/Essential/Core/Scripts/Utils/ApplicationQuit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Essential.Core.Utils
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// MonoBehaviour providing a method to quit the application.
+	/// </summary>
+	public class ApplicationQuit : MonoBehaviour {
+
+		/// <summary>
+		/// Quit application even when started in Unity Editor.
+		/// </summary>
+		public void Quit ()
+		{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+		}
+	}
+}
