f34009b992d6cfed37d30fa9fc3fd97f8bb330fa
Lars Wobus <lars.wobus@web.de>
2018-09-21 21:49:06 +0200

    Unit tests for toggling components

--- EssentialCore/Assets/Essential/Core/Scripts/Utils/ToggleComponents.cs
+++ EssentialCore/Assets/Essential/Core/Scripts/Utils/ToggleComponents.cs
@@ -1,5 +1,10 @@
 ﻿using UnityEngine;
 
+#if UNITY_EDITOR
+using System;
+using System.Linq;
+#endif
+
 namespace Essential.Core.Utils
 {
 	/// <inheritdoc />
@@ -16,6 +21,27 @@ namespace Essential.Core.Utils
 		/// </summary>
 		[SerializeField] private Component[] _components;
 		
+#if UNITY_EDITOR
+		
+		/// <summary>
+		/// Check if public field was set properly.
+		/// </summary>
+		/// <exception cref="ArgumentException">Throw when array remains empty on start.</exception>
+		/// <exception cref="NullReferenceException">Throw when one or more array elements are not assigned.</exception>
+		private void Start()
+		{
+			if (_components.Length == 0)
+			{
+				throw new ArgumentException("Array remains empty.");
+			}
+
+			if (_components.Any(element => element == null))
+			{
+				throw new NullReferenceException("Element in array was null");
+			}
+		}
+#endif
+		
 		/// <summary>
 		/// Toggle 'enabled' properties of persistent Components.
 		/// </summary>
@@ -29,8 +55,8 @@ namespace Essential.Core.Utils
 					continue;
 				}
 
-				var enabled = (bool) propertyInfo.GetValue(component, null);
-				propertyInfo.SetValue(component, !enabled);
+				var status = (bool) propertyInfo.GetValue(component, null);
+				propertyInfo.SetValue(component, !status);
 			}
 		}
 	}
---
+++ EssentialCore/Assets/Tests/Utils/Runtime/ToggleComponentsTest.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Reflection;
+using Essential.Core.Tests.Configuration;
+using Essential.Core.Utils;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests.Utils.Runtime
+{
+	public class ToggleComponentsTest : MonoBehaviour {
+		
+		private class DefaultBehaviour : MonoBehaviour {}
+
+		private GameObject DummyGameObject { get; set; }
+		private ToggleComponents TargetScript { get; set; }
+		private FieldInfo FieldInfo { get; set; }
+		
+		[SetUp]
+		public void Setup()
+		{
+			DummyGameObject = new GameObject();
+		}
+		
+		[UnityTest]
+		public IEnumerator Should_ThrowArgumentException_When_ComponentArrayRemainsEmpty()
+		{
+			TargetScript = DummyGameObject.AddComponent<ToggleComponents>();
+			FieldInfo = TargetScript.GetType().GetField("_components", BindingFlags.NonPublic | BindingFlags.Instance);
+		
+			FieldInfo.SetValue(TargetScript, new Component[]{});
+		
+			TestSettings.ExpectException("ArgumentException:");
+			yield return null;
+		}
+	
+		[UnityTest]
+		public IEnumerator Should_ThrowNullReferenceException_When_AtLeastOneComponentArrayElementIsNull()
+		{
+			TargetScript = DummyGameObject.AddComponent<ToggleComponents>();
+			FieldInfo = TargetScript.GetType().GetField("_components", BindingFlags.NonPublic | BindingFlags.Instance);
+		
+			var components = new Component[]{null};
+			FieldInfo.SetValue(TargetScript, components);
+		
+			TestSettings.ExpectException("NullReferenceException:");
+			yield return null;
+		}
+
+		[UnityTest]
+		public IEnumerator Should_ContainOnlyActiveComponents_When_ScriptFullyInitialized()
+		{
+			TargetScript = DummyGameObject.AddComponent<ToggleComponents>();
+			FieldInfo = TargetScript.GetType().GetField("_components", BindingFlags.NonPublic | BindingFlags.Instance);
+		
+			var monoBehaviour1 = new GameObject().AddComponent<DefaultBehaviour>();
+			var monoBehaviour2 = new GameObject().AddComponent<DefaultBehaviour>();
+			var components = new[]{monoBehaviour1, monoBehaviour2};
+			FieldInfo.SetValue(TargetScript, components);
+			
+			var propertyInfo = typeof(MonoBehaviour).GetProperty("enabled");
+			var monobehaviour1Status = (bool) propertyInfo.GetValue(monoBehaviour1, null);;
+			var monobehaviour2Status = (bool) propertyInfo.GetValue(monoBehaviour2, null);;
+			
+			var actual = monobehaviour1Status && monobehaviour2Status;
+			Assert.IsTrue(actual);
+			yield return null;
+		}
+
+		[UnityTest]
+		public IEnumerator Should_DisableAllComponents_When_ToggleMethodWasCalled()
+		{
+			TargetScript = DummyGameObject.AddComponent<ToggleComponents>();
+			FieldInfo = TargetScript.GetType().GetField("_components", BindingFlags.NonPublic | BindingFlags.Instance);
+		
+			var monoBehaviour1 = new GameObject().AddComponent<DefaultBehaviour>();
+			var monoBehaviour2 = new GameObject().AddComponent<DefaultBehaviour>();
+			var components = new[]{monoBehaviour1, monoBehaviour2};
+			FieldInfo.SetValue(TargetScript, components);
+
+			TargetScript.Toggle();
+			
+			var propertyInfo = typeof(MonoBehaviour).GetProperty("enabled");
+			var monobehaviour1Status = (bool) propertyInfo.GetValue(monoBehaviour1, null);;
+			var monobehaviour2Status = (bool) propertyInfo.GetValue(monoBehaviour2, null);;
+			
+			var actual = monobehaviour1Status || monobehaviour2Status;
+			Assert.IsFalse(actual);
+			yield return null;
+		}
+	}
+}
