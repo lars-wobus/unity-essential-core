3e532f7b539cfc202204b7ee7616872936887ac8
Lars Wobus <lars.wobus@web.de>
2018-09-22 23:01:16 +0200

    Print all exposed values from AudioMixer

---
+++ EssentialCore/Assets/Essential/Core/Scripts/Audio/VolumeControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Essential.Core.Audio
+{
+	public class VolumeControl : MonoBehaviour
+	{
+		[SerializeField] private AudioMixer _masterMixer;
+
+		private void Start () {
+			var parameters = _masterMixer.GetType().GetProperty("exposedParameters").GetValue(_masterMixer, null) as Array;
+			foreach (var element in parameters)
+			{
+				var a = (string)element.GetType().GetField("name").GetValue(element);
+				Debug.Log(a);
+			}
+		}
+	}
+}
