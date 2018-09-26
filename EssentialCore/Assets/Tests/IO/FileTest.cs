b7b34c132d5944f1bd997ed5e31d4d65177c8a4d
Lars Wobus <lars.wobus@web.de>
2018-09-26 21:58:28 +0200

    Update UnitTests for handling file IO

--- EssentialCore/Assets/Tests/IO/FileTest.cs
+++ EssentialCore/Assets/Tests/IO/FileTest.cs
@@ -13,38 +13,154 @@ namespace Tests.IO
             TestPath = Application.persistentDataPath;
         }
         
+        // File exist
+        
+        [Test]
+        public void Should_ReturnFalse_When_FilePathIsNull()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_FilePathIsEmpty()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_FileDoesNotExist()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_AnyExceptionIsThrownWhileCheckingTheExistenceOfFile()
+        {
+            Assert.Fail();
+        }
+        
         [Test]
         public void Should_ReturnTrue_When_FileExists()
         {
             Assert.Fail();
         }
         
+        // File delete
+        
         [Test]
-        public void Should_ReturnFalse_When_FileNotExists()
+        public void Should_ReturnFalse_When_DeletingNull()
         {
             Assert.Fail();
         }
         
         [Test]
-        public void Should_DeleteFile_When_FileExists()
+        public void Should_ReturnFalse_When_DeletingEmptyString()
         {
             Assert.Fail();
         }
         
         [Test]
-        public void Should_NotDeleteAnything_When_FileNotExists()
+        public void Should_ReturnFalse_When_DeletingNonExistingFile()
         {
             Assert.Fail();
         }
         
         [Test]
-        public void Should_MoveFile_When_FileNotAlreadyExistInTargetDirectory()
+        public void Should_ReturnFalse_When_AnyExceptionIsThrownWhileDeletingFile()
         {
             Assert.Fail();
         }
         
         [Test]
-        public void Should_NotMoveFile_When_FileAlreadyExistInTargetDirectory()
+        public void Should_ReturnTrue_When_FileCouldBeDeleted()
+        {
+            Assert.Fail();
+        }
+        
+        // File move
+        
+        [Test]
+        public void Should_ReturnFalse_When_MovingNull()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_MovingFileToNull()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_MovingEmptyString()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_MovingFileToEmptyString()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_IdenticallyNamedFileInTargetDirectoryBlocksFileMovement()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_AnyExceptionIsThrownWhileMovingFile()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnTrue_When_FileCouldBeMoved()
+        {
+            Assert.Fail(); 
+        }
+        
+        // File copy
+        
+        [Test]
+        public void Should_ReturnFalse_When_CopyingNull()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_CopyingFileToNull()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_CopyingEmptyString()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_CopyingFileToEmptyString()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_TargetDirectoryContainsFileWithSameNameAsCopy()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_AnyExceptionIsThrownWhileCopyingFile()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnTrue_When_FileCouldBeCopied()
         {
             Assert.Fail(); 
         }
