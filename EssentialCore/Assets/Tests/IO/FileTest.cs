339db898376e3dfca6b37d6aa185e1631c73ed5e
Lars Wobus <lars.wobus@web.de>
2018-09-26 20:57:41 +0200

    Prepare UnitTests for file handling methods

---
+++ EssentialCore/Assets/Tests/IO/FileTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.IO
+{
+    public class FileTest
+    {
+        private string TestPath { get; set; }
+        
+        [SetUp]
+        public void Setup()
+        {
+            TestPath = Application.persistentDataPath;
+        }
+        
+        [Test]
+        public void Should_ReturnTrue_When_FileExists()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_ReturnFalse_When_FileNotExists()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_DeleteFile_When_FileExists()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_NotDeleteAnything_When_FileNotExists()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_MoveFile_When_FileNotAlreadyExistInTargetDirectory()
+        {
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void Should_NotMoveFile_When_FileAlreadyExistInTargetDirectory()
+        {
+            Assert.Fail(); 
+        }
+    }
+}
