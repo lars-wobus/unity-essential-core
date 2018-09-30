196527af4e61bf883ad65bdd7a0612d281bd216a
Lars Wobus <lars.wobus@web.de>
2018-09-30 09:52:29 +0200

    Start implementing Directory class

---
+++ EssentialCore/Assets/Essential/Core/Scripts/IO/Directory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Directory
+{
+    public static bool Exists(string path)
+    {
+        return false;
+    }
+
+    public static bool Create(string path)
+    {
+        return false;
+    }
+
+    public static bool Delete(string path)
+    {
+        return false;
+    }
+
+    public static bool Clean(string path)
+    {
+        return false;
+    }
+}
--- EssentialCore/Assets/Tests/IO/DirectoryTest.cs
+++ EssentialCore/Assets/Tests/IO/DirectoryTest.cs
@@ -6,11 +6,33 @@ namespace Tests.IO
     public class DirectoryTest
     {
         private string TestPath { get; set; }
+        private string InvalidCharacters { get; set; }
+        private string TestFilePath { get; set; }
+        private string TestFilePathTarget { get; set; }
         
         [SetUp]
         public void Setup()
         {
             TestPath = Application.persistentDataPath;
+            InvalidCharacters = new string(System.IO.Path.GetInvalidPathChars());
+
+            TestFilePath = TestPath + "/FileForUnitTesting.txt";
+            TestFilePathTarget = TestPath + "/FileForUnitTesting.txt";
+            using(var streamWriter = new System.IO.StreamWriter(TestFilePath, true))
+            {
+                streamWriter.WriteLine("Some dummy text.");
+            }
+            using(var streamWriter = new System.IO.StreamWriter(TestFilePathTarget, true))
+            {
+                streamWriter.WriteLine("Some dummy text.");
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            System.IO.File.Delete(TestFilePath);
+            System.IO.File.Delete(TestFilePathTarget);
         }
         
         // Directory exist
@@ -18,30 +40,40 @@ namespace Tests.IO
         [Test]
         public void Should_ReturnFalse_When_DirectoryPathIsNull()
         {
+            var acutal = Directory.Exists(null);
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_DirectoryPathIsEmpty()
         {
+            var acutal = Directory.Exists("");
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_DirectoryPathContainsInvalidCharacters()
         {
+            var acutal = Directory.Exists(InvalidCharacters);
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_DirectoryPathPointsToFile()
         {
+            var acutal = Directory.Exists(TestFilePath);
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_DirectoryDoesNotExist()
         {
+            var acutal = Directory.Exists(Application.persistentDataPath + "/nonExistingFolder");
+            
             Assert.Fail();
         }
         
@@ -54,6 +86,8 @@ namespace Tests.IO
         [Test]
         public void Should_ReturnTrue_When_DirectoryExists()
         {
+            var acutal = Directory.Exists(Application.persistentDataPath);
+            
             Assert.Fail();
         }
         
@@ -62,24 +96,32 @@ namespace Tests.IO
         [Test]
         public void Should_ReturnFalse_When_CreatingNull()
         {
+            var acutal = Directory.Create(null);
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_CreatingEmptyString()
         {
+            var acutal = Directory.Create("");
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_CreatingDirectoryContainingInvalidCharacters()
         {
+            var acutal = Directory.Create(InvalidCharacters);
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_TryingToCreateFile()
         {
+            var acutal = Directory.Create(TestFilePath);
+            
             Assert.Fail();
         }
         
@@ -92,12 +134,16 @@ namespace Tests.IO
         [Test]
         public void Should_ReturnTrue_When_CreatingExistingDirectory()
         {
+            var acutal = Directory.Create(Application.persistentDataPath + "/existingEmptyDirectory");
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnTrue_When_CreatingNonExistingDirectory()
         {
+            var acutal = Directory.Create(Application.persistentDataPath + "/nonExistingDirectory");
+            
             Assert.Fail();
         }
         
@@ -106,30 +152,40 @@ namespace Tests.IO
         [Test]
         public void Should_ReturnFalse_When_DeletingNull()
         {
+            var acutal = Directory.Delete(null);
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_DeletingEmptyString()
         {
+            var acutal = Directory.Delete("");
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_DeletingDirectoryContainingInvalidCharacters()
         {
+            var acutal = Directory.Delete(InvalidCharacters);
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_TryingToDeleteFile()
         {
+            var acutal = Directory.Delete(TestFilePath);
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_DeletingNonExistingDirectory()
         {
+            var acutal = Directory.Delete(Application.persistentDataPath + "/nonExistingDirectory");
+            
             Assert.Fail();
         }
         
@@ -142,12 +198,16 @@ namespace Tests.IO
         [Test]
         public void Should_ReturnFalse_When_DeletingNonEmptyDirectory()
         {
+            var acutal = Directory.Delete(Application.persistentDataPath + "/existingNonEmptyDirectory");
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnTrue_When_DirectoryCouldBeDeleted()
         {
+            var acutal = Directory.Delete(Application.persistentDataPath + "/existingEmptyDirectory");
+            
             Assert.Fail();
         }
         
@@ -156,30 +216,40 @@ namespace Tests.IO
         [Test]
         public void Should_ReturnFalse_When_CleaningNull()
         {
+            var acutal = Directory.Clean(null);
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_CleaningEmptyString()
         {
+            var acutal = Directory.Clean("");
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_CleaningDirectoryContainingInvalidCharacters()
         {
+            var acutal = Directory.Clean(InvalidCharacters);
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_TryingToCleanFile()
         {
+            var acutal = Directory.Clean(TestFilePath);
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnFalse_When_CleaningNonExistingDirectory()
         {
+            var acutal = Directory.Clean(Application.persistentDataPath + "/nonExistingDirectory");
+            
             Assert.Fail();
         }
         
@@ -192,18 +262,24 @@ namespace Tests.IO
         [Test]
         public void Should_ReturnFalse_When_FileRemainsAfterCleaningDirectory()
         {
+            var acutal = Directory.Clean(Application.persistentDataPath + "/existingNonEmptyDirectory");
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnTrue_When_CleaningEmptyDirectory()
         {
+            var acutal = Directory.Clean(Application.persistentDataPath + "/existingEmptyDirectory");
+            
             Assert.Fail();
         }
         
         [Test]
         public void Should_ReturnTrue_When_CleaningNonEmptyDirectory()
         {
+            var acutal = Directory.Clean(Application.persistentDataPath + "/existingNonEmptyDirectory");
+            
             Assert.Fail();
         }
     }
