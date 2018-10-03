377a1ac7fe55c799130f50055a4def3c1edb6d7c
Lars Wobus <lars.wobus@web.de>
2018-10-03 22:18:38 +0200

    Code cleanup + some notes for further evaluation

--- EssentialCore/Assets/Essential/Core/Scripts/Memory/GameDataRecovery.cs
+++ EssentialCore/Assets/Essential/Core/Scripts/Memory/GameDataRecovery.cs
@@ -2,37 +2,38 @@
 
 namespace Essential.Core.Memory
 {
+	// Note: Why not implementing methods from Orginator here? Because of the 'single responsible principle'. 
+	// Moreover multiple versions of the originator might be required someday.
+	// So why not inherting from an abstract version of Originator with type parameter which inherits from MonoBehaviour?
+	// Because something like this: 'public class MementoBehaviour<TBehaviour, TData> : MonoBehaviour' would force us
+	// to implement another interface for TBehaviour, to have access to the Data object.
+	// Another possible solution could be 'public class MementoBehaviour<TBehaviour<TData>>'. Right now I didn't 
+	// spend enought time to evaluate this strategy.
+	
 	[RequireComponent(typeof(GameDataRestorer))]
 	public class GameDataRecovery : MonoBehaviour
 	{
-		private GameDataRestorer _script;
-		private Caretaker<GameDataRestorer.Data> _caretaker;
-		private Originator<GameDataRestorer.Data> _originator = new Originator<GameDataRestorer.Data>();
+		private readonly Originator<GameDataRestorer.Data> _originator = new Originator<GameDataRestorer.Data>();
+		private GameDataRestorer TargetScript { get; set; }
+		private Caretaker<GameDataRestorer.Data> Caretaker { get; set; }
 		
 		// Use this for initialization
 		private void Start ()
 		{
-			_script = GetComponent<GameDataRestorer>();
-			_originator.CurrentState = _script.Data2;
+			TargetScript = GetComponent<GameDataRestorer>();
+			_originator.CurrentState = TargetScript.Data2;
 		}
 	
 		private void OnApplicationFocus(bool hasFocus)
 		{
-			Debug.Log("OnApplicationFocus " + hasFocus);
-			Debug.Log(_originator.CurrentState.a);
 			if (hasFocus)
 			{
-				//_data = _originator.CurrentState;
-				_script.Data2 = _originator.RestoreFromMomento(_caretaker.Memento);
-				Debug.Log("Use state from originator");
+				TargetScript.Data2 = _originator.RestoreFromMomento(Caretaker.Memento);
 			}
 			else
 			{
-				_caretaker = new Caretaker<GameDataRestorer.Data>(_originator.SaveToMemento());
-				Debug.Log("Save state through" +
-				          " originator");
+				Caretaker = new Caretaker<GameDataRestorer.Data>(_originator.SaveToMemento());
 			}
-			Debug.Log(_originator.CurrentState.a);
 		}
 
 		/*private void OnApplicationPause(bool pauseStatus)
