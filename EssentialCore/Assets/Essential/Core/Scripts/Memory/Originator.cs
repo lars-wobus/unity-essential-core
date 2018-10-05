f9216e311457452eef2e43e14da862eb4912bb58
Lars Wobus <lars.wobus@web.de>
2018-10-05 19:54:37 +0200

    C# Doc for the Originator (memento pattern)

--- EssentialCore/Assets/Essential/Core/Scripts/Memory/Originator.cs
+++ EssentialCore/Assets/Essential/Core/Scripts/Memory/Originator.cs
@@ -4,18 +4,33 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Essential.Core.Memory
 {
-	// Note: The advantage of an originator: it can hold further data, e.g. load/save counter, etc.
-	
+	/// <summary>
+	/// Allows to save and restore states.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public class Originator<T> where T : class 
 	{
+		/// <summary>
+		/// Current internal state.
+		/// </summary>
 		public T CurrentState { get; set; }
+		// place for additional data that is not part of the state being saved in memento, e.g. load/save counter, etc.
 
+		/// <summary>
+		/// Create memento that stores the originator's current internal state.
+		/// </summary>
+		/// <returns>Copy of its own internal state.</returns>
 		public T SaveToMemento()
 		{
 			// ReSharper disable once HeapView.BoxingAllocation
 			return CurrentState == null ? CurrentState : _serialize(CurrentState);
 		}
 
+		/// <summary>
+		/// Restore internal state from passed memento.
+		/// </summary>
+		/// <param name="storedInstance">Any internal state from the past.</param>
+		/// <returns>Internal state after adopting or rejecting the input.</returns>
 		public T RestoreFromMomento(T storedInstance)
 		{
 			if (storedInstance == null)
@@ -26,6 +41,9 @@ namespace Essential.Core.Memory
 			return CurrentState;
 		}
 
+		/// <summary>
+		/// Create copy of your current internal state. 
+		/// </summary>
 		private readonly Func<T, T> _serialize = objectInstance =>
 		{
 			var memoryStream = new MemoryStream();
