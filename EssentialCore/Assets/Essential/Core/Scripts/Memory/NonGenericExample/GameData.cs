c23c91da44dcdd2dc7fa7cfd7bce3bd27d9bdef5
Lars Wobus <lars.wobus@web.de>
2018-10-05 21:04:59 +0200

    C# docs updated

--- EssentialCore/Assets/Essential/Core/Scripts/Memory/NonGenericExample/GameData.cs
+++ EssentialCore/Assets/Essential/Core/Scripts/Memory/NonGenericExample/GameData.cs
@@ -9,8 +9,14 @@ namespace Essential.Core.Memory.NonGenericExample
 	[Serializable]
 	public class GameData
 	{
+		/// <summary>
+		/// Single number describing the internal state of a behavioural script at a specific moment.
+		/// </summary>
 		[SerializeField] private int _index;
 
+		/// <summary>
+		/// Get/Set index.
+		/// </summary>
 		public int Index
 		{
 			get { return _index; }
