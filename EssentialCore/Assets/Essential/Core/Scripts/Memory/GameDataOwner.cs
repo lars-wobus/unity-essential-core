c69ace04f9f4f1337b0649d5a3cfe57e955184e8
Lars Wobus <lars.wobus@web.de>
2018-10-05 20:26:58 +0200

    C# doc for the GameDataOwner behaviour

--- EssentialCore/Assets/Essential/Core/Scripts/Memory/GameDataOwner.cs
+++ EssentialCore/Assets/Essential/Core/Scripts/Memory/GameDataOwner.cs
@@ -3,22 +3,37 @@ using UnityEngine;
 
 namespace Essential.Core.Memory
 {
+    /// <summary>
+    /// Example behaviour. Its internal state can be restored by another behaviour. 
+    /// </summary>
     public class GameDataOwner : MonoBehaviour
     {
+        /// <summary>
+        /// Specifies the internal state of behavioural classes.
+        /// </summary>
         [Serializable]
         public class Data
         {
             public int a;
         }
 
+        /// <summary>
+        /// Internal state.
+        /// </summary>
         [SerializeField] private Data _data;
 
+        /// <summary>
+        /// Get/Set internal state.
+        /// </summary>
         public Data Data2
         {
             get { return _data; }
             set { _data = value; }
         }
 
+        /// <summary>
+        /// Update is used to constantly modify own state. 
+        /// </summary>
         public void Update()
         {
             _data.a++;
