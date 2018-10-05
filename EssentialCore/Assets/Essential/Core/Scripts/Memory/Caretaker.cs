e56ba0ae95abdd6633f0733e1ce833248ca9f062
Lars Wobus <lars.wobus@web.de>
2018-10-05 19:59:25 +0200

    C# Doc for the Caretaker (memento pattern)

--- EssentialCore/Assets/Essential/Core/Scripts/Memory/Caretaker.cs
+++ EssentialCore/Assets/Essential/Core/Scripts/Memory/Caretaker.cs
@@ -1,9 +1,20 @@
 ﻿namespace Essential.Core.Memory
 {
+    /// <summary>
+    /// Allows to store a single internal state of an Originator.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Caretaker<T>
     {
+        /// <summary>
+        /// Get saved internal state of an Originator.
+        /// </summary>
         public T Memento { get; }
     
+        /// <summary>
+        /// While creating a new instance, the passed internal state of an Originator will be saved.
+        /// </summary>
+        /// <param name="objectInstance">Current internal state of an Originator.</param>
         public Caretaker(T objectInstance)
         {
             Memento = objectInstance;
