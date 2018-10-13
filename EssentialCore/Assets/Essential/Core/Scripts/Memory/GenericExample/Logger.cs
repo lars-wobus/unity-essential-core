30e9bdb8e94a280cb6c89619a0c3803de66848cb
Lars Wobus <lars.wobus@web.de>
2018-10-13 21:12:33 +0200

    Simple logger to test Monitoring behaviours

---
+++ EssentialCore/Assets/Essential/Core/Scripts/Memory/GenericExample/Logger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Essential.Core.Memory.GenericExample
+{
+	public class Logger : MonoBehaviour
+	{
+		public void WriteOnConsole(string text)
+		{
+			Debug.Log(text);
+		}
+	}
+}
