2646d675431ae68ae58057b9ac753dd9c3b8f727
Lars Wobus <lars.wobus@web.de>
2018-10-17 21:13:01 +0200

    Add another example to test enums

---
+++ EssentialCore/Assets/Essential/Core/Scripts/Memory/Examples/SimpleEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Essential.Core.Scripts.Memory.Examples
+{
+	public enum SimpleEnum
+	{
+		Neutral = 0,
+		Friendly = 1,
+		Aggressive = 2
+	}
+}
---
+++ EssentialCore/Assets/Essential/Core/Scripts/Memory/Examples/SimpleEnumOwner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using Essential.Core.Memory;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Essential.Core.Scripts.Memory.Examples
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// 
+	/// </summary>
+	public class SimpleEnumOwner : DataOwnerBase<SimpleEnum>
+	{
+		private readonly Random _random = new Random();
+
+		protected override IEnumerator UpdateValues()
+		{
+			while (true)
+			{
+				Data = (SimpleEnum) _random.Next(0, 3);
+				Debug.Log(Data);
+				
+				yield return new WaitForSeconds(SecondsToWait);
+			}
+		}
+	};
+} 
