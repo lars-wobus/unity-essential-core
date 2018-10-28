4dfd96aa5d8672284cd6b1841176e8f99cbccadb
Lars Wobus <lars.wobus@web.de>
2018-10-28 14:46:49 +0100

    Update C# doc

--- EssentialCore/Assets/Essential/Core/Scripts/Localization/SupportedLanguages.cs
+++ EssentialCore/Assets/Essential/Core/Scripts/Localization/SupportedLanguages.cs
@@ -1,12 +1,32 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Globalization;
 using UnityEngine;
 
 namespace Essential.Core.Localization
 {
+	/// <inheritdoc />
+	/// <summary>
+	/// Scriptable object to store an array of culture identifiers.
+	/// </summary>
 	public class SupportedLanguages : ScriptableObject
 	{
+		/// <summary>
+		/// Specifies set of possible identifiers.
+		/// </summary>
 		[SerializeField] private CultureTypes _activeCultureType;
+		
+		/// <summary>
+		/// Array of supported culture identifiers which can be set via the Unity inspector. 
+		/// </summary>
 		[SerializeField] private string[] _identifiers;
+
+		/// <summary>
+		/// Get culture type which defines the subset of possible identifiers during the development.
+		/// </summary>
+		public CultureTypes CultureType => _activeCultureType;
+		
+		/// <summary>
+		/// Get array of supported culture identifiers.
+		/// </summary>
+		public string[] Identifiers => _identifiers;
 	}
 }
