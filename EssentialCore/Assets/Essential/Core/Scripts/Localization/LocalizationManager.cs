01100023e7c5c02668a3ad9e464d374255f4317f
Lars Wobus <lars.wobus@web.de>
2018-10-29 22:25:24 +0100

    Code cleanup

--- EssentialCore/Assets/Essential/Core/Scripts/Localization/LocalizationManager.cs
+++ EssentialCore/Assets/Essential/Core/Scripts/Localization/LocalizationManager.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Essential.Core.IO;
 using Essential.Core.Localization.Data;
 using Essential.Core.Localization.Interfaces;
@@ -12,15 +11,11 @@ namespace Essential.Core.Localization
 		[SerializeField] private StreamingAssetsPathSubfolder _rootFolder = StreamingAssetsPathSubfolder.Localization;
 		[SerializeField] private string _sampleFile = "settings_de.json";
 		[SerializeField] private string _activeLanguage = "de";
-		
-		//private Dictionary<string, string> Language { get; set; }
+
 		private Dictionary<string, Language> Languages { get; set; }
-		
-	
-		// Use this for initialization
+
 		private void Awake ()
 		{
-			//Language = new Dictionary<string, string>();
 			Languages = new Dictionary<string, Language>();
 			Languages.Add(_activeLanguage, new Language());
 
@@ -44,7 +39,6 @@ namespace Essential.Core.Localization
 			
 			foreach (var item in localizationData.items)
 			{
-				//Language.Add(item.id, item.text);
 				vocabulary.Add(item.id, item.text);
 			}
 		}
@@ -54,14 +48,12 @@ namespace Essential.Core.Localization
 			var id = localizedText.Id;
 
 			var vocabulary = Languages[_activeLanguage].Vocabulary;
-			
-			//if (!Language.ContainsKey(id))
+
 			if(!vocabulary.ContainsKey(id))
 			{
 				return;
 			}
 
-			//var value = Language[id];
 			var value = vocabulary[id];
 			localizedText.SetText(value);
 		}
