c1e0ea1eb535c072ae58b89d7650894ca7a6f17e
Lars Wobus <lars.wobus@web.de>
2018-10-31 08:40:33 +0100

    Start reorderable list for scenes

---
+++ EssentialCore/Assets/Essential/Core/Scripts/SceneManagement/Editor/SceneCollectionInspector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Essential.Core.SceneManagement.Editor
+{
+	[CustomEditor(typeof(SceneCollection))]
+	public class SceneCollectionInspector : UnityEditor.Editor {
+
+		/// <summary>
+		/// Get/Set ReorderableList.
+		/// </summary>
+		private ReorderableList ReorderableList { get; set; }
+		
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+			ReorderableList.DoLayoutList();
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		private void OnEnable()
+		{
+			ReorderableList = new ReorderableList(serializedObject, serializedObject.FindProperty("_scenes"), true, true, true, true);
+			ReorderableList.drawElementCallback = (Rect rect, int listIndex, bool isAvtive, bool isFocused) =>
+			{
+				var element = ReorderableList.serializedProperty.GetArrayElementAtIndex(listIndex);
+				rect.y += 2;
+			};
+		}
+	}
+}
---
+++ EssentialCore/Assets/Essential/Core/Scripts/SceneManagement/SceneCollection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Essential.Core.SceneManagement
+{
+    public class SceneCollection : MonoBehaviour
+    {
+        [SerializeField] private UnityScene[] _scenes;
+    }
+}
