4a5039e7b95cc55da275a53443d9b838172e09ca
Lars Wobus <lars.wobus@web.de>
2018-11-05 22:27:03 +0100

    Default application close dialog

---
+++ EssentialCore/Assets/Essential/Core/Scripts/UI/UiCanvasVisibility.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Essential.Core.Scripts.UI
+{
+	public class UiCanvasVisibility : MonoBehaviour
+	{
+		[SerializeField] private Canvas _targetCanvas;
+		[SerializeField] private bool _isVisible = true;
+
+		public bool IsVisible
+		{
+			get{
+				return _isVisible;
+			}
+			set
+			{
+				if (_isVisible == value)
+				{
+					return;
+				}
+
+				_isVisible = value;
+
+				OnValueChanged();
+			}
+		}
+
+		private void Start ()
+		{
+			OnValueChanged();
+		}
+
+		private void OnValueChanged()
+		{
+			_targetCanvas.enabled = IsVisible;
+		}
+	}
+}
